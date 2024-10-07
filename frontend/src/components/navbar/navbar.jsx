b5176abd405aed37b668b1ec98a9dddca02fbf7e
Italo Fabrício <italofps65@gmail.com>
2024-10-07 10:43:52 -0300

    create routes and start creating login and signup

--- frontend/src/components/navbar/navbar.jsx
+++ frontend/src/components/navbar/navbar.jsx
@@ -2,6 +2,7 @@ import styles from "./navbar.module.css";
 import { LuShoppingCart, LuUserCircle, LuMenu } from "react-icons/lu";
 import { Drawer } from "@mui/material";
 import { useState } from "react";
+import { Link } from "react-router-dom";
 
 export default function Navbar() {
   const [openMenu, setOpenMenu] = useState(false);
@@ -13,23 +14,35 @@ export default function Navbar() {
   return (
     <nav className={styles.navbarContainer}>
       <div className={styles.navbarItems}>
-        <img className={styles.logo} src="/logo.png" alt="" />
+        <Link to={'/'}>
+          <img className={styles.logo} src="/logo.png" alt="" />
+        </Link>
+
         <div className={styles.navbarLinksContainer}>
-          <a href="" className={styles.navbarLink}>
-            Home
-          </a>
-          <a href="" className={styles.navbarLink}>
-            Plates
-          </a>
-          <LuShoppingCart className={styles.navbarLink} />
-          <LuUserCircle className={styles.navbarLink} />
+          <Link to={'/'}>
+            <a href="" className={styles.navbarLink}>Home</a>
+          </Link>
+          <Link to={'/plates'}>
+            <a href="" className={styles.navbarLink}>Plates</a>
+          </Link>
+          <Link to={'/cart'}>
+            <LuShoppingCart className={styles.navbarLink} />
+          </Link>
+          <Link to={'/profile'}>
+            <LuUserCircle className={styles.navbarLink} />
+          </Link>
+          
         </div>
       </div>
 
       <div className={styles.mobileNavbarItems}>
-        <img className={styles.logo} src="/logo.png" alt="" />
+        <Link to={'/'}>
+          <img className={styles.logo} src="/logo.png" alt="" />
+        </Link>
         <div className={styles.mobileNavbarBtns}>
-          <LuShoppingCart className={styles.navbarLink} />
+          <Link to={'/cart'}>
+            <LuShoppingCart className={styles.navbarLink} />
+          </Link>
           <LuMenu className={styles.navbarLink} onClick={handleOpenMenu} />
         </div>
       </div>
@@ -41,15 +54,15 @@ export default function Navbar() {
         
       >
         <div className={styles.drawer}>
-          <a href="" className={styles.navbarLink}>
-            Home
-          </a>
-          <a href="" className={styles.navbarLink}>
-            Plates
-          </a>
-          <a href="" className={styles.navbarLink}>
-            Perfil
-          </a>
+          <Link to={'/'} onClick={handleOpenMenu}>
+            <a href="" className={styles.navbarLink}>Home</a>
+          </Link>
+          <Link to={'/plates'} onClick={handleOpenMenu}>
+            <a href="" className={styles.navbarLink}>Plates</a>
+          </Link>
+          <Link to={'/profile'} onClick={handleOpenMenu}>
+            <a href="" className={styles.navbarLink}>Perfil</a>
+          </Link>
         </div>
       </Drawer>
     </nav>
---
+++ frontend/src/pages/auth/page.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+export default function Auth () {
+    const [formType, setFormType] = useState('login');
+
+    const handleChangeFormType = () => {
+        if(formType === 'login') {
+            setFormType('signup')
+        } else {
+            setFormType('login')
+        }
+    }
+
+    if(formType === 'login') {
+        return (
+            <>
+                <h1>Login</h1>
+                <button onClick={handleChangeFormType}>Don't you have an account ? Click here</button>
+            </>
+        )
+    }
+
+    if(formType === 'signup') {
+        return (
+            <>
+                <h1>Signup</h1>
+                <button onClick={handleChangeFormType}>Already have an account ? Click here</button>
+            </>
+        )
+    }
+    
+}
