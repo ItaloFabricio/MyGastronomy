f135f6bdd19e5b6bed99939efec5ebfb9600f65c
Italo Fabrício <italofps65@gmail.com>
2024-10-07 11:06:18 -0300

    functions for form and inputs in login

--- frontend/src/pages/auth/page.jsx
+++ frontend/src/pages/auth/page.jsx
@@ -1,7 +1,10 @@
 import { useState } from "react";
+import { Button, TextField } from "@mui/material";
+import styles from "./page.module.css";
 
 export default function Auth () {
     const [formType, setFormType] = useState('login');
+    const [formData, setFormData] = useState(null);
 
     const handleChangeFormType = () => {
         if(formType === 'login') {
@@ -11,12 +14,42 @@ export default function Auth () {
         }
     }
 
+    const handleFormDataChange = (e) => {
+        setFormData({
+            ...formData,
+            [e.target.name]: e.target.value
+        })
+    }
+
+    const handleSubmitForm = (e) => {
+        e.preventDefault()
+        console.log(formData)
+    }
+
     if(formType === 'login') {
         return (
-            <>
+            <div className={styles.authPageContainer}>
                 <h1>Login</h1>
                 <button onClick={handleChangeFormType}>Don't you have an account ? Click here</button>
-            </>
+                <form onSubmit={handleSubmitForm}>
+                    <TextField
+                        required
+                        label='Email'
+                        type="email"
+                        name="email"
+                        onChange={handleFormDataChange}
+                    />
+
+                    <TextField
+                        required
+                        label="Password"
+                        type="password"
+                        name="password"
+                        onChange={handleFormDataChange}
+                    />
+                    <Button type="submit">Login</Button>
+                </form>
+            </div>
         )
     }
 
