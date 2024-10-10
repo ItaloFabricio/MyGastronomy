1412a092d5362fd2c89b17d0a91f2a3714365bf6
Italo Fabrício <italofps65@gmail.com>
2024-10-10 16:10:47 -0300

    geting order by user from the api rest

--- frontend/src/pages/profile/page.jsx
+++ frontend/src/pages/profile/page.jsx
@@ -1,28 +1,64 @@
 import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import authService from "../../services/auth";
+import orderServices from "../../services/order";
+import styles from './page.module.css';
+
 
 export default function Profile () {
     const { logout } = authService(); 
+    const { getUserOrders, orderLoading, refetchOrders, ordersData} = orderServices();
     const navigate = useNavigate();
     const authData = JSON.parse(localStorage.getItem('auth'));
 
     useEffect(() => {
         if(!authData) {
             return navigate('/auth')
+        } else if (refetchOrders) {
+            getUserOrders(authData?.user?._id)
         }
-    }, [authData])
+    }, [authData, refetchOrders])
+
+    if(orderLoading) {
+        return( <h1>Loading...</h1>)
+    } 
 
     const handleLogout = () => {
         logout();
         return navigate('/');
     }
 
+    console.log(ordersData);
+
     return (
         <>
             <h1>{authData?.user?.fullname}</h1>
             <h3>{authData?.user?.email}</h3>
             <button onClick={handleLogout}>Logout</button>
+
+            <div>
+                
+            </div>
+            {ordersData.length > 0 ? 
+                <div className={styles.orderContainer}>
+                    {ordersData.map((order) => (
+                        <div key={order._id}>
+                            <p>{order.pickupStatus}</p>
+                            <h3>{order.pickupTime}</h3>
+                            {order.orderItems.map((item) => (
+                                <div key={item._id}>
+                                    <h4>{item.itemDetails[0].name}</h4>
+                                    <p>Quantity: {item.quantity}</p>
+                                </div>
+                            ))}
+                        </div>
+                    ))}
+                </div>
+            :
+                <div>
+                    You do not have orders yet
+                </div>
+            }
         </>
     )
 }
---
+++ frontend/src/services/order.jsx
@@ -0,0 +1,42 @@
+import { useState } from "react";
+
+export default function orderServices() {
+    //elemento de carregamento da pagina
+    const [orderLoading, setOrderLoading] = useState(false);
+    
+    //fazer a requisição novamente das orders
+    const [refetchOrders, setRefetchOrders] = useState(true);
+
+    const [ordersData, setOdersData] = useState([]);
+
+    const url = 'http://localhost:3000/orders';
+
+    const getUserOrders = (userId) => {
+        setOrderLoading(true);
+        
+        fetch(`${url}/userOrders/${userId}`, {
+            method: 'GET',
+            headers: {
+                'Content-Type': 'application/json',
+                'Access-Control-Allow-Origin': '*'
+            },
+        })
+        .then((response) => response.json())
+        .then((result) => {
+            if(result.success) {
+                setOdersData(result.body)
+            } else {
+                console.log(result)
+            }
+        })
+        .catch((error) => {
+            console.log(error)
+        })
+        .finally(() => {
+            setOrderLoading(false)
+            setRefetchOrders(false)
+        })
+    }
+
+    return { getUserOrders, orderLoading, refetchOrders, ordersData }
+}
